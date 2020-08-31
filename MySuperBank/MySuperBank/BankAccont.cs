063ad5ca9dbc8d6b5bfb723c6f8cff6515fc0734
ashraf-mortazavi <ashrafmortazavi18@gmail.com>
2020-08-31 15:29:40 +0430

    new change in ignore file and mean file

---
+++ MySuperBank/MySuperBank/BankAccont.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+   public  class BankAccont
+    {
+        public string  Number {get;}
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in alltransection)
+                {
+                    balance += item.Amount;
+                }
+                return balance; 
+            }
+                   
+                
+          }
+
+        private static int accountnumber = 123456789;
+
+        private List<Transaction> alltransection = new List<Transaction>();
+        public BankAccont(string name, decimal initialbalance)
+        {
+            this.Owner = name;
+            Makedeposit(initialbalance, DateTime.Now, "save initialbalance as the deposit");
+            this.Number = accountnumber.ToString();
+            accountnumber++;
+        }
+
+        public void Makedeposit (decimal amount, DateTime date, string note)
+        {
+            if (amount <=0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "amount of deposite must be positive"); 
+            }
+            var deposite = new Transaction(amount, date, note);
+            alltransection.Add(deposite);
+        }
+        public void MakeWitheral (decimal amount, DateTime date, string note)
+        {
+         if(amount <=0 )
+            {
+                throw new ArgumentOutOfRangeException (nameof(amount), "amount of witheral must be positive");
+
+            }
+         if (Balance - amount <0)
+            {
+                throw new InvalidOperationException("Not Sufficient funds for this witherwal");
+            }
+            var witheral = new Transaction(-amount, date, note);
+            alltransection.Add(witheral);
+
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+            // Header 
+            report.AppendLine("Date\t Amount \t Note");
+
+            //ROWS
+            foreach (var item in alltransection)
+            {
+                report.AppendLine($"{item.Date.ToShortDateString()}\t {item.Amount} \t {item.Notes}");
+            }
+
+            return report.ToString();
+        
+        }
+       
+    }
+}
---
+++ MySuperBank/MySuperBank/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccont("ashraf",2000);
+            //var account2 = new BankAccont("amin", 5000);
+            //Console.WriteLine($"this account is:{account.Owner} with the value:{account.Balance} and number: {account.Number}");
+            //Console.WriteLine($"this is another account:{account2.Owner} with the value :{account2.Balance} and number: {account2.Number}");
+
+            account.Makedeposit(500, DateTime.Now, "jzgjd");
+            account.MakeWitheral(250, DateTime.Now, "fhfrtet");
+            Console.WriteLine(account.Balance);
+            Console.WriteLine(account.GetAccountHistory());
+           
+
+            //try
+            //{
+            //    account.MakeWitheral(5000, DateTime.Now, "ftyrytyu");
+
+            //}
+            //catch (InvalidOperationException e )
+            //{
+            //    Console.WriteLine("Exception caught trying to overdraw");
+            //    Console.WriteLine(e.ToString());
+            //}
+           
+            //try
+            //{
+            //    var x = new BankAccont("ahmad", -60);
+            //}
+            //catch (ArgumentOutOfRangeException e )
+            //{
+            //    Console.WriteLine("Exception cought creating account with negative value");
+            //    Console.WriteLine(e.ToString());
+
+            //}
+
+           
+
+        }
+    }
+}
